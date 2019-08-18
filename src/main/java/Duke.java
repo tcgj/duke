5d29a6e169fe206e0b672271b9aa8b7ef4947c85
tcgj <terenceblur0410@gmail.com>
2019-08-19 01:30:07 +0800

    Make duke respond to Level-1 requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String helloText = "Hello! I'm Duke\nWhat can I do for you?";
+        System.out.println(helloText);
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            boolean willExit = false;
+            String input = sc.nextLine();
+
+            switch(input) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    willExit = true;
+                    break;
+                default:
+                    System.out.println(input);
+                    break;
+            }
+
+            if (willExit) {
+                break;
+            }
+        }
+        sc.close();
     }
 }
