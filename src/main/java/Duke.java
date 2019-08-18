ee00d0fa72c31c7f02b77843e547a4692edf40e4
tcgj <terenceblur0410@gmail.com>
2019-08-19 02:00:20 +0800

    Level up Duke to Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    private ArrayList<String> list;
+
+    public Duke() {
+        list = new ArrayList<>();
+    }
+
+    public void run() {
         String helloText = "Hello! I'm Duke\nWhat can I do for you?";
         System.out.println(helloText);
 
@@ -15,8 +22,12 @@ public class Duke {
                     System.out.println("Bye. Hope to see you again soon!");
                     willExit = true;
                     break;
+                case "list":
+                    displayList();
+                    break;
                 default:
-                    System.out.println(input);
+                    addToList(input);
+                    System.out.println("added: " + input);
                     break;
             }
 
@@ -25,5 +36,21 @@ public class Duke {
             }
         }
         sc.close();
+    }
+
+    private void displayList() {
+        int listSize = list.size();
+        for (int i = 0; i < listSize; i++) {
+            System.out.println((i + 1) + ". " + list.get(i));
+        }
+    }
+
+    private void addToList(String item) {
+        list.add(item);
+    }
+
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.run();
     }
 }
