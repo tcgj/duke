5573640e3c1aae6293ff4a82fd71bdc6bfeea419
tcgj <terenceblur0410@gmail.com>
2019-09-01 14:22:39 +0800

    Update automated testing files

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,14 +13,19 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src -Xlint:none -d ../bin ../src/main/java/*.java
+cd ../src/main/java
+if ! javac -Xlint:none -d ../../../bin duke/Duke.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
+cd ../../../text-ui-test
+
+# clear previous data
+rm -r data
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+java -classpath ../bin duke.Duke < input.txt > ACTUAL.TXT
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED.TXT
