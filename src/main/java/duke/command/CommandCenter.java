afdde66c70f668625c77af99ab39ebc46acd7386
tcgj <terenceblur0410@gmail.com>
2019-09-11 19:27:41 +0800

    Create command aliases

--- src/main/java/duke/command/CommandCenter.java
+++ src/main/java/duke/command/CommandCenter.java
@@ -11,19 +11,21 @@ public class CommandCenter {
     static {
         commands = new HashMap<>();
         paramsList = new HashMap<>();
-        registerCommand("todo", TodoCommand::new, new String[0]);
-        registerCommand("event", EventCommand::new, new String[]{"/at"});
-        registerCommand("deadline", DeadlineCommand::new, new String[]{"/by"});
-        registerCommand("find", FindCommand::new, new String[0]);
-        registerCommand("list", ListCommand::new, new String[0]);
-        registerCommand("done", DoneCommand::new, new String[0]);
-        registerCommand("delete", DeleteCommand::new, new String[0]);
-        registerCommand("bye", ByeCommand::new, new String[0]);
+        registerCommand(TodoCommand::new, new String[0], "todo", "t");
+        registerCommand(EventCommand::new, new String[]{"/at"}, "event", "e");
+        registerCommand(DeadlineCommand::new, new String[]{"/by"}, "deadline", "dead");
+        registerCommand(FindCommand::new, new String[0], "find", "f", "search");
+        registerCommand(ListCommand::new, new String[0], "list", "l");
+        registerCommand(DoneCommand::new, new String[0], "done", "d", "completed", "complete");
+        registerCommand(DeleteCommand::new, new String[0], "delete", "del");
+        registerCommand(ByeCommand::new, new String[0], "bye", "b");
     }
 
-    private static void registerCommand(String str, Function<List<String>, ? extends Command> ctr, String[] params) {
-        commands.put(str, ctr);
-        paramsList.put(str, params);
+    private static void registerCommand(Function<List<String>, ? extends Command> ctr, String[] params, String... keywords) {
+        for (String keyword : keywords) {
+            commands.put(keyword, ctr);
+            paramsList.put(keyword, params);
+        }
     }
 
     public static Function<List<String>, ? extends Command> getCommand(String str) {
